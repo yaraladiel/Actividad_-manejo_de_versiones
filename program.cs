32897a77b512bb41dce4ba1b778c817f61047c56
yaraladiel <yaraladiel@gmail.com>
2026-03-19 22:33:14 -0400

    Enhance output with branch2 information
    Added additional output statements to indicate branch usage.

--- program.cs
+++ program.cs
@@ -5,7 +5,11 @@ namespace program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello word");
+   Console.WriteLine("Hello word");          
+ Console.Write(" From branch1");    
+ Console.Write(" and branch2");
+             Console.WriteLine("I'm writing from here");    
+            
         }
     }
 }
